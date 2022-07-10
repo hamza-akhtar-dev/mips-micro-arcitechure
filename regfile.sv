845e32c203e538e4e0a01762cd418f8daf5a7f37
Hamza Akhtar <hamzaakhtar8812@gmail.com>
2022-07-10 22:12:50 +0500

    Test Instructions Added

--- regfile.sv
+++ regfile.sv
@@ -18,8 +18,8 @@ logic [31:0] rf[31:0];
 always_ff @(posedge clk)
     if (we3) rf[wa3] <= wd3;
 
-assign rd1 = (ra1 ! = 0) ? rf[ra1] : 0;
+assign rd1 = (ra1 != 0) ? rf[ra1] : 0;
 
-assign rd2 = (ra2 ! = 0) ? rf[ra2] : 0;
+assign rd2 = (ra2 != 0) ? rf[ra2] : 0;
 
 endmodule
